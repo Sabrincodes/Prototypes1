4732999b8ec0d87b0965f5f41e4650c3dadfd11b
sashajustice <sabrinnasreddin@gmail.com>
2018-09-26 18:43:01 -0600

    complete first prototype

---
+++ main.js
@@ -0,0 +1,67 @@
+
+1. Given the following dataset, return an array of just the front-end classrooms
+let classrooms = [
+  { roomLetter: 'A', program: 'FE', capacity: 32 },
+  { roomLetter: 'B', program: 'BE', capacity: 29 },
+  { roomLetter: 'C', program: 'FE', capacity: 27 },
+  { roomLetter: 'D', program: 'BE', capacity: 30 },
+  { roomLetter: 'E', program: 'FE', capacity: 22 },
+  { roomLetter: 'F', program: 'BE', capacity: 19 },
+  { roomLetter: 'G', program: 'FE', capacity: 29 },
+  { roomLetter: 'H', program: 'BE', capacity: 18 }
+];
+
+
+ var newClass = classrooms.filter(class => {
+   if(classroom.program === 'FE') {
+     return classRoom
+   }
+
+
+
+
+const artists = [
+  { name: 'SG Lewis', label: 'atlantic' },
+  { name: 'HONNE', label: 'virgin' },
+  { name: 'BORNS', label: 'capital' },
+  { name: 'Leisure', label: 'capital' },
+  { name: 'St. Lucia', label: 'capital' },
+  { name: 'Gallant', label: 'capital' },
+  { name: 'Marcus Marr', label: 'virgin' },
+  { name: 'Penguin Prison', label: 'atlantic' },
+  { name: 'Future Islands', label: 'atlantic' },
+  { name: 'Roosevelt', label: 'capital' },
+  { name: 'Jack Garratt', label: 'capital' },
+  { name: 'Chet Faker', label: 'virgin' },
+  { name: 'Sam Sparro', label: 'virgin' }
+];
+
+const ticketPrice = {
+  atlantic: 20,
+  virgin: 25,
+  capital: 30
+};
+
+
+// Return an array of all the artists
+
+
+
+// Write a function that takes a name as a parameter and returns the first artist where the name matches .  ex. findByName('Jack Garratt')
+
+
+
+// Write some code to add the appropriate ticket price to each artist.
+
+
+
+// Write a function that takes a label as a parameter and returns an array containing all the artists of that label .  ex. getByLabel('atlantic')
+
+
+
+// Write some code to determine how much it would cost to go to all to the shows
+
+
+
+
+// Make an object containing each label as keys and all the associated artists in an array as the value.
